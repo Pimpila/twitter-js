3b9cb8d9bb29639041a29e204a761d6b650804a1
MarcosRS <MarcosRS16@gmail.com>
2017-03-07 14:13:46 -0500

    update

--- app.js
+++ app.js
@@ -14,36 +14,15 @@ var locals = {
 
 nunjucks.configure('views', {noCache: true});
 
-// nunjucks.render('index.html', locals, function (err, output) {
-//     console.log(output);
-// });
-
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
 app.use(function(req, res, next) {
-    // log method and path props on req obj
-    // log statusCode prop on res obj
- //    res.render('index', locals, function(err, html) {
- //  		res.send(html);
-	// });
+    
 	res.render( 'index', locals);
-    console.log(req.method, req.path, res.statusCode);
-    // next();
+    
 });
 
-// this logs anytime a client request begins with /special (can't be in middle of route)
-// app.use('/special', function(req, res, next) {
-//     console.log('you have reached a special area')
-// })
-
-// app.get('/', function(req, res, next) {
-//     res.send('welcome!')
-// })
-
-// app.get('/news', function(req, res, next) {
-//     res.send('welcome to news')
-// })
 
 app.listen(3000, function() {
     console.log('server listening')
